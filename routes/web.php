91ba78a69f2e09bf939144d71946e29782ee0690
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-02-28 15:49:13 +0100

    miscellaneous

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.') 
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+            ->name('home');
+        Route::resource('posts', 'PostController');
+    });
