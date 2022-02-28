2c97da3ea992694f95af9d48200737ab93916a51
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-02-28 16:57:54 +0100

    add fake data to posts db

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use Faker\Generator as Faker;
+use App\Model\Post;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
@@ -9,8 +12,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 120; $i++) { 
+            $newPost = new Post();
+            $newPost->title = $faker->words(9, true);
+            $newPost->content = $faker->paragraph(5, true);
+            $newPost->slug = Str::slug($newPost->title . "-$i", '-');
+
+            $newPost->save();
+        }
     }
 }
