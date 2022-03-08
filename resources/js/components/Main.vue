498f5767e101f6509cc54985446e68898c9d6d1a
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-03-08 19:01:00 +0100

    adjust card in Main.vue

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
-        <div class="card" v-for="(post, index) in posts" :key="index">
-            <img v-if="post.image" :src="post.image" class="card-img-top" :alt="post.title">
-            <div class="card-body">
-                <h3 class="card-title">{{ post.title }}</h3>
-                <p class="card-text">{{ post.content }}</p>
+    <div class="row row-cols-4 g-3 p-3">
+        <div class="col" v-for="(post, index) in posts" :key="index">
+                <div class="card h-100">
+                <img v-if="post.image" :src="post.image" class="card-img-top" :alt="post.title">
+                <div class="card-body">
+                    <h3 class="card-title">{{ post.title }}</h3>
+                    <p class="card-text">{{ post.content }}</p>
+                </div>
+                <!-- <ul class="list-group list-group-flush">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                </ul>
+                <div class="card-body">
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div> -->
             </div>
-            <!-- <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
-            <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div> -->
         </div>
     </div>
 </template>
