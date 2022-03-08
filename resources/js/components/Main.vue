04d09e8f2b8a240a5bff88afc6198a8a8fe6f154
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-03-08 18:58:05 +0100

    add v-for in main.vue

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="card" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-        </ul>
-        <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+    <div>
+        <div class="card" v-for="(post, index) in posts" :key="index">
+            <img v-if="post.image" :src="post.image" class="card-img-top" :alt="post.title">
+            <div class="card-body">
+                <h3 class="card-title">{{ post.title }}</h3>
+                <p class="card-text">{{ post.content }}</p>
+            </div>
+            <!-- <ul class="list-group list-group-flush">
+                <li class="list-group-item">An item</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div> -->
         </div>
     </div>
 </template>
@@ -22,10 +24,15 @@ import Axios from 'axios';
 
 export default {
     name:"Main",
+    data(){
+        return {
+            posts: [],
+        }
+    },
     created() {
         Axios.get('http://127.0.0.1:8000/api/posts')
-             .then((result) => 
-                console.log(result)
+             .then((result) =>
+                this.posts = result.data.result.posts
              )
              .catch()
 
