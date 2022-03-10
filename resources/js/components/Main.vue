52f5dfb8938316aea4ea76850f7e7a7f8f3dbf15
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-03-10 18:20:35 +0100

    add props Main.vue from Home.vue

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row row-cols-4 g-3 p-3">
-        <div class="col" v-for="(post, index) in posts" :key="index">
+        <div class="col" v-for="(post, index) in cards.posts" :key="index">
                 <div class="card h-100">
                 <img v-if="post.image" :src="'/storage/' + post.image" class="card-img-top" :alt="post.title">
                 <div class="card-body">
@@ -11,8 +11,8 @@
         </div>
         <div class="row w-100 justify-content-around mt-3 mb-1">
             <div class="col-2 d-flex justify-content-around">
-                <button v-if="prev_page" class="btn btn-success" @click="changePage('prev_page')">Prev</button>
-                <button v-if="next_page" class="btn btn-success" @click="changePage('next_page')">Next</button>
+                <button v-if="cards.prev_page" class="btn btn-success" @click="changePage('prev_page')">Prev</button>
+                <button v-if="cards.next_page" class="btn btn-success" @click="changePage('next_page')">Next</button>
             </div>
         </div>
     </div>
@@ -23,42 +23,7 @@ import Axios from 'axios';
 
 export default {
     name:"Main",
-    data(){
-        return {
-            posts: [],
-            prev_page: null,
-            next_page: null,
-
-        }
-    },
-    created() {
-        
-        Axios.get('http://127.0.0.1:8000/api/posts')
-             .then((result) => {
-                    this.posts = result.data.result.data;
-                    this.prev_page = result.data.result.prev_page_url;
-                    this.next_page = result.data.result.next_page_url;
-                }
-             )
-             .catch()
-
-    },
-    methods: {
-        changePage(pageUrl) {
-            let url = this[pageUrl];
-
-            if (url) {
-                Axios.get(url)
-                .then((result) => {
-                    this.posts = result.data.result.data;
-                    this.prev_page = result.data.result.prev_page_url;
-                    this.next_page = result.data.result.next_page_url;
-                    }
-                )
-                .catch()
-            }
-        }
-    }
+    props: ['cards'],
 }
 </script>
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,12 +1,63 @@
 <template>
   <div>
-      <h1>Home</h1>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col">
+                  <h1>Home</h1>
+              </div>
+          </div>
+          <Main :cards='cards'></Main>
+      </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+import Main from '../components/Main';
+
 export default {
     name: 'Home',
+    components: {
+        Main,
+    },
+    
+    data(){
+        return {
+            cards: {    
+                posts: [],
+                prev_page: null,
+                next_page: null,
+            },
+        }
+    },
+    created() {
+        
+        Axios.get('http://127.0.0.1:8000/api/posts')
+             .then((result) => {
+                    this.cards.posts = result.data.result.data;
+                    this.cards.prev_page = result.data.result.prev_page_url;
+                    this.cards.next_page = result.data.result.next_page_url;
+                }
+             )
+             .catch()
+
+    },
+    methods: {
+        changePage(pageUrl) {
+            let url = this[pageUrl];
+
+            if (url) {
+                Axios.get(url)
+                .then((result) => {
+                    this.posts = result.data.result.data;
+                    this.prev_page = result.data.result.prev_page_url;
+                    this.next_page = result.data.result.next_page_url;
+                    }
+                )
+                .catch()
+            }
+        }
+    }
 }
 </script>
 
